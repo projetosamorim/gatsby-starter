bdfc4fe90f94a480b35c611fae7c9a5400b15e32
projetosamorim <amorim.rm@hotmail.com>
2023-01-16 14:58:32 -0300

    Publishing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import * as React from "react"
+import {Link} from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <div style={{color: `purple`}}>
+    <Link to="/contact">Contact</Link>
+    <Link to="/about">About</Link>
+    <Header headerText="Gatsby Developer" />
+    <p>Seja bem-vindo!</p>
+    <img src= "https://sourse.unsplash.com/random/400x200" alt= ""
+/>
+  </div>
+  )
 }
